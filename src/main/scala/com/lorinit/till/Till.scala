4016cb09203d498cbedbaf6c4142848b9573a719
cihanuluisik <cihanuluisik@hotmail.com>
2017-03-21 22:19:19 +0000

    step 3 - tiny renaming

--- src/main/scala/com/lorinit/till/Till.scala
+++ src/main/scala/com/lorinit/till/Till.scala
@@ -10,7 +10,7 @@ class Till(priceList: Item => Int, offers: Item => Offer) {
   @tailrec
   private def calculate(acc: mutable.Map[Item, Int], list: List[Item]): Int = list match {
     case Nil if acc.isEmpty => 0
-    case Nil => acc.foldLeft(0)((a, b) => a + priceList(b._1) * b._2 - (b._2 / offers(b._1).threshold) * offers(b._1).discountAmount)
+    case Nil => acc.foldLeft(0)((subtotal, kv) => subtotal + priceList(kv._1) * kv._2 - (kv._2 / offers(kv._1).threshold) * offers(kv._1).discountAmount)
     case item :: tail =>  acc.put(item, acc.getOrElseUpdate(item, 0) + 1); calculate(acc, tail)
   }
 
