38c1c7663e56d4d16cc495a40d1684190e23092a
cihanuluisik <cihanuluisik@hotmail.com>
2017-04-01 17:27:35 +0100

    step 3 - Till made a bit more readable

--- src/main/scala/com/lorinit/till/Till.scala
+++ src/main/scala/com/lorinit/till/Till.scala
@@ -3,15 +3,19 @@ package com.lorinit.till
 import scala.annotation.tailrec
 import scala.collection.mutable
 
-class Till(priceList: Item => Int, offers: Item => Offer) {
+class Till(price: Item => Int, offer: Item => Offer) {
 
   def calculate(shoppingList: List[Item]) :Int = calculate(mutable.Map(), shoppingList)
 
   @tailrec
   private def calculate(acc: mutable.Map[Item, Int], list: List[Item]): Int = list match {
     case Nil if acc.isEmpty => 0
-    case Nil => acc.foldLeft(0)((subtotal, kv) => subtotal + priceList(kv._1) * kv._2 - (kv._2 / offers(kv._1).threshold) * offers(kv._1).discountAmount)
+    case Nil => (for ((item, count) <- acc ) yield price(item) * count - discountTotal(item, count)).sum
     case item :: tail =>  acc.put(item, acc.getOrElseUpdate(item, 0) + 1); calculate(acc, tail)
+  }
+
+  private def discountTotal(item: Item, count: Int): Int = {
+    count / offer(item).threshold * offer(item).discountAmount
   }
 
 }
